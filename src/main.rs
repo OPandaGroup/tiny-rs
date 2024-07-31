8fa5cda7aab6b2ef049eba0c2eb066fd6d7a65c7
砸松果 <zasongguo@outlook.com>
2024-07-31 21:53:26 +0800

    Add the function of processing images.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use iced::widget::text_editor::Content;
 use std::fs;
 use std::path::PathBuf;
 
@@ -8,13 +7,12 @@ use iced::widget::{column, container, Button, Text, TextInput};
 use iced::{executor, Application, Command, Settings};
 use rfd::FileDialog;
 use tinify::async_bin::Tinify;
+// static mut
 // use tinify::error::TinifyError;
 struct App {
     path: Vec<PathBuf>,
     api_key_val: String,
-    tinify: Tinify,
-    // api_key_is_ok: bool,
-    content: Content,
+    // content: Content,
     warn_text: Option<String>,
 }
 #[derive(Debug, Clone)]
@@ -40,6 +38,7 @@ enum Message {
     AddSth(AddSth),
     ClearPath,
     Convert,
+    WarnTextUpdated(Option<String>),
 }
 impl Application for App {
     type Executor = executor::Default;
@@ -48,15 +47,11 @@ impl Application for App {
     type Theme = iced::Theme;
     fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
         let path = Vec::new();
-        let tinify = Tinify::new();
-        // let api_key_is_ok = false;
-        let content = Content::new();
         (
             Self {
-                content,
+                // content,
                 path,
                 api_key_val: "".into(),
-                tinify,
                 // api_key_is_ok,
                 warn_text: None,
             },
@@ -68,12 +63,11 @@ impl Application for App {
             Message::AddSth(addsth) => {
                 match addsth {
                     AddSth::APi(api_key) => {
-                        self.tinify = Tinify::new().set_key(api_key);
+                        self.api_key_val = api_key;
                     }
                     AddSth::Path => {
-                        let paths = image_files(
-                            FileDialog::new().pick_folder().unwrap(), //todo: handle Err Better?
-                        );
+                        let paths =
+                            image_files(FileDialog::new().pick_folder().unwrap_or_default());
                         let iter1 = paths.into_iter();
                         let iter2 = self.path.clone().into_iter();
                         self.path = iter1.chain(iter2).collect();
@@ -83,9 +77,33 @@ impl Application for App {
             }
 
             Message::Convert => {
-                todo!()
+                let path = self.path.clone();
+                let tinify: Tinify = Tinify::new().set_key(&self.api_key_val);
+
+                Command::perform(
+                    async move {
+                        process_image(
+                            tinify,
+                            path.iter()
+                                .map(|p| (*p.to_string_lossy()).to_owned())
+                                .collect(),
+                        )
+                        .await
+                    },
+                    |result| {
+                        if let Err(e) = result {
+                            Message::WarnTextUpdated(Some(e.to_string()))
+                        } else {
+                            Message::WarnTextUpdated(None)
+                        }
+                    },
+                )
             }
             Message::ClearPath => Command::none(),
+            Message::WarnTextUpdated(warn) => {
+                self.warn_text = warn;
+                Command::none()
+            }
         }
     }
 
@@ -97,8 +115,11 @@ impl Application for App {
         let column = column!(
             TextInput::new("API Key", &self.api_key_val)
                 .on_input(|s: String| Message::AddSth(AddSth::APi(s)))
+                .on_paste(|s: String| Message::AddSth(AddSth::APi(s)))
                 .padding(40),
-            Button::new("ClearPath").on_press(Message::ClearPath)
+            Button::new("AddPath").on_press(Message::AddSth(AddSth::Path)),
+            Button::new("ClearPath").on_press(Message::ClearPath),
+            Button::new("Process").on_press(Message::Convert)
         );
 
         let column = if let Some(text) = &self.warn_text {
@@ -111,34 +132,36 @@ impl Application for App {
     }
 }
 
-impl App {
-    async fn process_image(&self, path: Vec<String>, api_key: &str) {
-        for p in path {
-            let new_file_name = p
-                .split("/")
-                .last()
-                .unwrap()
-                .split(".")
-                .to_owned()
-                .collect::<Vec<_>>()[0]
-                .to_string()
-                + "optimized"; // a.png -> a-optimized.png
+async fn process_image(tinify: Tinify, path: Vec<String>) -> anyhow::Result<()> {
+    for p in path {
+        let mut new_file_name = Vec::new();
+        let mut optimized_string_is_added = false;
 
-            self.tinify
-                .get_async_client()
-                .unwrap()
-                .from_file(p.clone())
-                .await
-                .unwrap()
-                .to_file(new_file_name)
-                .await;
-        }
+        p.split(".").for_each(|i| {
+            if !i.is_empty() && !optimized_string_is_added {
+                new_file_name.push(format!("{i}-optimized"));
+                optimized_string_is_added = true;
+            } else {
+                new_file_name.push(i.to_string());
+            }
+        });
+
+        let new_file_name = new_file_name.join(".");
+
+        tinify
+            .get_async_client()?
+            .from_file(p.clone())
+            .await?
+            .to_file(new_file_name.clone())
+            .await?;
+
+        println!("{new_file_name} processed!");
     }
+    Ok(())
 }
 
-fn image_files(path: PathBuf) -> Vec<PathBuf> {
-    let iter1 = fs::read_dir(&path)
-        .unwrap()
+fn image_files(path: PathBuf) -> anyhow::Result<Vec<PathBuf>> {
+    let iter1 = fs::read_dir(&path)?
         .map(|entry| entry.unwrap().path())
         .filter(|item| {
             item.is_file()
@@ -147,12 +170,11 @@ fn image_files(path: PathBuf) -> Vec<PathBuf> {
                     || item.to_str().unwrap().to_lowercase().ends_with(".webp"))
         });
 
-    let iter2 = fs::read_dir(path)
-        .unwrap()
+    let iter2 = fs::read_dir(path)?
         .map(|entry| entry.unwrap().path())
         .filter(|item| item.is_dir())
-        .flat_map(|item| image_files(item).into_iter());
-    iter1.chain(iter2).collect()
+        .flat_map(|item| image_files(item).unwrap().into_iter());
+    Ok(iter1.chain(iter2).collect())
 }
 
 fn main() {
